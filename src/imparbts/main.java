45842964ae5bcc99d186a90a40b7ecb7d6ecfd56
DESKTOP-4A7G64Q\User <user@etec.com>
2026-08-19 12:51:58 -0300

    adicionado arquivos do repositório

---
+++ src/imparbts/main.java
@@ -0,0 +1,22 @@
+package imparbts;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void main(String[] args) {
+			int i = 1;
+			int impar = 0;
+			while (i <= 25) {
+				int a = 1 + impar;
+				JOptionPane.showMessageDialog(null, a);
+			    i++;
+			    impar++;
+			    impar++;
+			   
+			}
+
+
+	}
+
+}
